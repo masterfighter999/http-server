260ea0d736b604eb4f4dce620999184ac2c14eb1
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-08 18:07:44 +0530

    Committed

--- httpServer/app/main.py
+++ httpServer/app/main.py
@@ -1,11 +1,9 @@
 import socket
 
 def main():
-
-    print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(('localhost', 4221),reuse_port=True)
-    server_socket.accept()
+    print("Logs from your program will appear here! ")
+    server_socket = socket.create_server(('', 8000))
+    server_socket.accept()  
 
 if __name__ == "__main__":
     main()
