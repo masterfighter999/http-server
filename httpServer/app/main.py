fba57508bb8df4851c6d885c11e5b555a42407e8
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-08 18:15:41 +0530

    [any message]

--- httpServer/app/main.py
+++ httpServer/app/main.py
@@ -4,9 +4,9 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible in the logs.
     print("Logs from your program will appear here! ")
     # Uncomment this to pass the first stage
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('', 4221))
-    server_socket.listen()  # wait for client
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+ 
+    server_socket.accept()  # wait for client
  
 
 if __name__ == "__main__":
