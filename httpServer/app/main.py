2cf1eb0c965a164da9d206eef5cc96b955231669
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-08 18:09:00 +0530

    Committed

--- httpServer/app/main.py
+++ httpServer/app/main.py
@@ -2,8 +2,16 @@ import socket
 
 def main():
     print("Logs from your program will appear here! ")
-    server_socket = socket.create_server(('', 8000))
-    server_socket.accept()  
+    server_socket = socket.create_server(('localhost', 4221), reuse_port=True)
+    with server_socket:
+        server_socket.listen()
+        conn, addr = server_socket.accept()  # Accept a connection
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
 
-if __name__ == "__main__":
-    main()
+main()
