0dce5f95413e74b3315e27243e9096d922771de1
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-08 18:10:00 +0530

    [any message]

--- httpServer/app/main.py
+++ httpServer/app/main.py
@@ -2,16 +2,8 @@ import socket
 
 def main():
     print("Logs from your program will appear here! ")
-    server_socket = socket.create_server(('localhost', 4221), reuse_port=True)
-    with server_socket:
-        server_socket.listen()
-        conn, addr = server_socket.accept()  # Accept a connection
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+    server_socket = socket.create_server(('', 8000))
+    server_socket.accept()  
 
-main()
+if __name__ == "__main__":
+    main()
