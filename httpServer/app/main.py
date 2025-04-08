0a749c8a6df026c7df9990da09361a207d80aef6
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-08 18:38:50 +0530

    [any message]

--- httpServer/app/main.py
+++ httpServer/app/main.py
@@ -2,9 +2,8 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
-    # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    server_socket.accept()  
 
 if __name__ == "__main__":
     main()
