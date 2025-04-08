6ddcc1fd3ecf20711a6c450582d34b13aa765c44
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-08 19:01:58 +0530

    [any message]

--- httpServer/app/main.py
+++
@@ -1,8 +0,0 @@
-
-
-def main():
-    print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  
-
-
