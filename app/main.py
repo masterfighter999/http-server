9f0fc031c65c8c12b3e82ef2ad2476d754a6f847
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-08 19:56:45 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,8 +8,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-
+    connection, address = server_socket.accept()  # wait for client
+    data = connection.recv(1024)
+    data_readable = data.decode().split("\r\n")
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    if data_readable[0].split(" ")[1] != "/":
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    connection.sendall(response)
 
 if __name__ == "__main__":
     main()
