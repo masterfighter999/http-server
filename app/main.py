48797ffacb8227cfdb1cacf911a59164eb9b552e
Swayam Chatterjee <masterfighter999@gmail.com>
2025-04-10 21:12:34 +0530

    gzip compression

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-# Uncomment this to pass the first stage
 import socket  # For creating server sockets
 import threading  # For handling multiple client connections concurrently
 import sys  # For accessing command-line arguments
+import gzip  # For gzip compression
 
 def main():
     # Function to handle individual client requests
@@ -21,9 +21,9 @@ def main():
                     response = "HTTP/1.1 200 OK\r\n\r\n".encode()
                 elif path.startswith('/echo'):
                     # Check if the "Accept-Encoding" header is present in the request
-                    if req[2].startswith("Accept-Encoding"):
+                    if req[4].startswith("Accept-Encoding"):
                         # Extract the encoding type from the "Accept-Encoding" header
-                        encoding = req[2].split(": ")[1]
+                        encoding = req[4].split(": ")[1]
                         print(encoding)  # Log the encoding type for debugging purposes
                         if encoding == 'gzip':
                             # Respond with gzip-encoded content if the encoding is "gzip"
